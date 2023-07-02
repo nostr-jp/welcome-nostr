512a783f81fbc163983ee98a0d65cc610af90cd4
penpenpng <poman1638m@gmail.com>
2023-07-02 22:41:04 +0900

    Use vuepress

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,48 @@
+import { defineUserConfig, defaultTheme } from "vuepress";
+import attrs from "markdown-it-attrs";
+
+export default defineUserConfig({
+  // To host on GitHub Pages.
+  base: "/welcome-nostr/",
+  lang: "ja_JP",
+  title: "Welcome to Nostr!",
+  // Ban README.md. Use index.md instead.
+  pagePatterns: ["**/*.md", "!**/README.md", "!.vuepress", "!node_modules"],
+  extendsMarkdown: (md) => {
+    // Allow `# Header Text {#custom-id-attr}` syntax.
+    md.use(attrs);
+  },
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: "GitHub",
+        link: "https://github.com/nostr-jp/welcome-nostr",
+      },
+    ],
+    sidebar: {
+      "/": [
+        {
+          text: "チュートリアル",
+          children: [
+            "/tutorial/set-profile-image.md",
+            "/tutorial/make-your-timeline.md",
+            "/tutorial/explore-client.md",
+            "/tutorial/nip-05.md",
+          ],
+        },
+        {
+          text: "基礎知識",
+          children: ["/guide/key-pair.md", "/guide/relay.md"],
+        },
+        {
+          text: "補遺",
+          children: ["/faq.md", "/trouble-shooting.md", "/learn-more.md"],
+        },
+      ],
+    },
+    editLinkText: "このページを編集",
+    docsRepo: "https://github.com/nostr-jp/welcome-nostr",
+    docsBranch: "main",
+    docsDir: "docs",
+  }),
+});
---
+++ docs/learn-more.md
@@ -0,0 +1,15 @@
+# もっと詳しく知りたい
+
+以下の資料が参考になるかもしれません
+
+- [Nostr Scrapbox](https://scrapbox.io/nostr/)
+  - 有志日本ユーザによって編集されている Nostr なんでも情報ノート。個人の日記から最新仕様の考察までなんでもござれ。
+  - 特に、はじめましての人は[こちら](https://scrapbox.io/nostr/%E3%81%AF%E3%81%98%E3%82%81%E3%81%A6%E3%81%AENostr%E3%80%90%E3%81%AF%E3%81%98%E3%82%81%E3%81%A6%E3%81%AE%E6%96%B9%E3%81%AF%E3%81%93%E3%81%A1%E3%82%89%E3%80%91)
+- [Nostr プロトコル(damus)を触ってみた - Qiita](https://qiita.com/gpsnmeajp/items/77eee9535fb1a092e286)
+  - Nostr の技術的な概要について簡潔にまとまっています
+- Nostr 勉強会: Nostr のはじめ方からサービス開発方法まで幅広く扱っている勉強会。動画とスライドが残っています。
+  - #0: https://428lab.connpass.com/event/275748/
+  - #1: https://428lab.connpass.com/event/276333/
+  - #2: https://428lab.connpass.com/event/278107/
+- [nostr-jp/awesome-nostr-japan](https://github.com/nostr-jp/awesome-nostr-japan)
+  - 日本ユーザによって作られた Nostr 関連ツール/サービスの一覧
